895e48a88440eadc2bf012dfb357731142d36bcf
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-05-23 12:23:34 -0700

    dependency injection

--- EcommerceProject/Client/Program.cs
+++ EcommerceProject/Client/Program.cs
@@ -1,4 +1,6 @@
 using EcommerceProject.Client;
+using EcommerceProject.Client.Services.ImplementationServices;
+using EcommerceProject.Client.Services.InterfaceServices;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
